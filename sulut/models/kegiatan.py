34d4102c42dab37efcc701aaf1872261dc238a0a
FWA-Bagasta <bagastasng@gmail.com>
2025-07-22 19:54:40 +0700

    Update kegiatan.py

--- sulut/models/kegiatan.py
+++ sulut/models/kegiatan.py
@@ -6,6 +6,7 @@ class Kegiatan(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'Master Data Kegiatan'
     # _rec_name = 'code'
+    _order = 'code asc'
     
     @api.depends('name', 'code')
     def name_get(self):
